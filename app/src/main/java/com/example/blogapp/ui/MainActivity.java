0fd654f2017f61673044954501ae503c08c48781
khammami <khammami@gmail.com>
2019-12-14 18:58:05 +0100

    replaced "word" by "post"

--- app/src/main/java/com/example/blogapp/ui/MainActivity.java
+++ app/src/main/java/com/example/blogapp/ui/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_DATA_UPDATE_DATE = "extra_date_to_be_updated";
     public static final String EXTRA_DATA_ID = "extra_data_id";
 
-    private PostViewModel mWordViewModel;
+    private PostViewModel mPostViewModel;
     private AlertDialog.Builder mAlertBuilder;
 
     @Override
@@ -55,14 +55,14 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Set up the WordViewModel.
-        mWordViewModel = ViewModelProviders.of(this).get(PostViewModel.class);
-        // Get all the words from the database
+        // Set up the PostViewModel.
+        mPostViewModel = ViewModelProviders.of(this).get(PostViewModel.class);
+        // Get all the posts from the database
         // and associate them to the adapter.
-        mWordViewModel.getAllPosts().observe(this, new Observer<List<Post>>() {
+        mPostViewModel.getAllPosts().observe(this, new Observer<List<Post>>() {
             @Override
             public void onChanged(@Nullable final List<Post> posts) {
-                // Update the cached copy of the words in the adapter.
+                // Update the cached copy of the posts in the adapter.
                 adapter.setPosts(posts);
             }
         });
@@ -99,8 +99,8 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     @Override
-                    // When the use swipes a word,
-                    // delete that word from the database.
+                    // When the use swipes a post,
+                    // delete that post from the database.
                     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
                         final int position = viewHolder.getAdapterPosition();
                         final Post mPost = adapter.getPostAtPosition(position);
@@ -115,10 +115,10 @@ public class MainActivity extends AppCompatActivity {
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog, int which) {
                                         Toast.makeText(MainActivity.this,
-                                                getString(R.string.delete_word_preamble) + " " +
+                                                getString(R.string.delete_post_preamble) + " " +
                                                         mPost.getTitle(), Toast.LENGTH_LONG).show();
-                                        // Delete the word.
-                                        mWordViewModel.deletePost(mPost);
+                                        // Delete the post.
+                                        mPostViewModel.deletePost(mPost);
                                     }
                                 });
 
@@ -166,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(getApplicationContext()
                                     , R.string.clear_data_toast_text, Toast.LENGTH_LONG).show();
                             // Delete the existing data.
-                            mWordViewModel.deleteAll();
+                            mPostViewModel.deleteAll();
                         }
                     });
 
@@ -193,7 +193,7 @@ public class MainActivity extends AppCompatActivity {
                                     data.getStringExtra(NewPostActivity.EXTRA_REPLY_CONTENT),
                                     Converters.fromTimestamp(data.getLongExtra(NewPostActivity.EXTRA_REPLY_DATE,0)));
             // Save the data.
-            mWordViewModel.insert(post);
+            mPostViewModel.insert(post);
         } else if (requestCode == UPDATE_POST_ACTIVITY_REQUEST_CODE
                 && resultCode == RESULT_OK) {
             String title_data = data.getStringExtra(NewPostActivity.EXTRA_REPLY_TITLE);
@@ -202,7 +202,7 @@ public class MainActivity extends AppCompatActivity {
             int id = data.getIntExtra(NewPostActivity.EXTRA_REPLY_ID, -1);
 
             if (id != -1) {
-                mWordViewModel.update(new Post(id, title_data,content_data,date_data));
+                mPostViewModel.update(new Post(id, title_data,content_data,date_data));
             } else {
                 Toast.makeText(this, R.string.unable_to_update,
                         Toast.LENGTH_LONG).show();
